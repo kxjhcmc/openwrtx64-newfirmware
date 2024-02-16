888f4e1fbf44850630249eb0a06a81a3f178960e
kxjhcmc <32127921+kxjhcmc@users.noreply.github.com>
2024-02-16 21:34:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # 修改openwrt登陆地址
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+#添加编译日期标识
+VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
+awk -v wrt_repo="$WRT_REPO" -v wrt_date="$WRT_DATE" '{ gsub(/(\(luciversion \|\| \047\047\))/, "& + (\047 / "wrt_repo"-"wrt_date"\047)") } 1' $VER_FILE > temp.js && mv -f temp.js $VER_FILE
