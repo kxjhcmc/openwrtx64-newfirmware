9649ddc1ff65f19c3ba12262bb8a8cd15f75f6d2
kxjhcmc <32127921+kxjhcmc@users.noreply.github.com>
2024-11-03 10:54:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,8 +19,8 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 rm -rf feeds/packages/net/chinadns-ng/Makefile
 wget -P feeds/packages/net/chinadns-ng https://raw.githubusercontent.com/xiaorouji/openwrt-passwall-packages/main/chinadns-ng/Makefile
 #替换xray
-rm -rf feeds/packages/net/xray-core/Makefile
-wget -P feeds/packages/net/xray-core https://raw.githubusercontent.com/kxjhcmc/openwrtx64-newfirmware/refs/heads/main/Makefile
+rm -rf feeds/packages/net/xray-core
+wget -P feeds/packages/net/xray-core https://raw.githubusercontent.com/xiaorouji/openwrt-passwall-packages/refs/heads/main/xray-core/Makefile
 # 替换miniupnpd
 #rm -rf feeds/packages/net/miniupnpd/Makefile
 #wget -P feeds/packages/net/miniupnpd https://raw.githubusercontent.com/openwrt/packages/master/net/miniupnpd/Makefile
