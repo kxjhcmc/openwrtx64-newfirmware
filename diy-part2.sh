2d7d2bc020661b89422b4934e68433183ee63f8f
kxjhcmc <32127921+kxjhcmc@users.noreply.github.com>
2025-03-08 13:58:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,6 +23,10 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwal
 rm -rf feeds/packages/net/miniupnpd
 git clone https://github.com/kxjhcmc/miniupnpd feeds/packages/net/miniupnpd
 
+# 总是拉取官方golang版本，避免xray&v2ray编译错误
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
 # 添加编译日期标识
 export DATE_VERSION=$(date +'%Y-%m-%d')
 sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
